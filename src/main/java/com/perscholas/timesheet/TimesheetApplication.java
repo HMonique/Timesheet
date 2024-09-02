b596348f0d53e0237b255c7fc73d17218375ab81
HMonique <161358207+HMonique@users.noreply.github.com>
2024-09-02 15:41:54 -0400

    third commit

--- src/main/java/com/perscholas/timesheet/TimesheetApplication.java
+++ src/main/java/com/perscholas/timesheet/TimesheetApplication.java
@@ -1,11 +1,29 @@
 package com.perscholas.timesheet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TimesheetApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(TimesheetApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(TimesheetApplication.class, args); }
+
+		SpringApplication.run(TimesheetApplication.class, args);
+	}
+
+	CommandLineRunner runner() {
+		return args -> {
+
+			logger.info("Timesheet Application up and running");
+			logger.warn("I am warning youuu !!!!!!");
+			logger.error("error");
+
+		};
+	}
+
 }
--- src/main/java/com/perscholas/timesheet/TimesheetController.java
+++ src/main/java/com/perscholas/timesheet/TimesheetController.java
@@ -1,7 +1,14 @@
 package com.perscholas.timesheet;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-
+@RestController
 public class TimesheetController {
+
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Timesheet Application";
+    }
 }
